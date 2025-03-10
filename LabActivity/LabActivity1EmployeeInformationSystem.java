0b1e319be9f9e4c4d4758e31c6dd1d91f2ad779d
Maricar An Mae Gallego <maricaranmae23@gmail.com>
2025-03-10 23:45:39 +0800

    Add files via upload

---
+++ LabActivity/LabActivity1EmployeeInformationSystem.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LabActivity1EmployeeInformationSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Getting the user's first name
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+        System.out.print("Enter hourly wage: ");
+        float hourlyWage = scanner.nextFLoat();
+
+        // Using the operation, hoursWorked and hourlyWage to compute the daily salary
+        double dailySalary = hoursWorked * hourlyWage;
+
+        // Output the Employee's Information
+        System.out.println("\nEmployee Information");
+        System.out.println("--------------------");
+        System.out.println("Full Name    : " + firstName + " " + lastName);
+        System.out.println("Age          : " + age + " years old");
+        System.out.printf("Daily Salary : PHP %.2f%n", dailySalary);
+
+        scanner.close();
+    }
+}
