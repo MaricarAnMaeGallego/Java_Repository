e56e9e714c01cdea3354a646be881a902dfe6057
Maricar An Mae Gallego <maricaranmae23@gmail.com>
2025-03-10 23:28:51 +0800

    Update LabAcivity2EmployeeInformationSystemPart2.java

--- LabAcivity2EmployeeInformationSystemPart2.java
+++ LabAcivity2EmployeeInformationSystemPart2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LabAcivity2EmployeeInformationSystemPart2 {
+public class LabActivity2EmployeeInformationSystemPart2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
